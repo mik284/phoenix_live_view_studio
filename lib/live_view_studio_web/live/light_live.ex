b8fc88a5a48403430096001ae0b75b0c1ce7064a
mik284 <mikeykamau222@gmail.com>
2023-12-19 10:13:28 +0300

    Handle On and Off Events

--- lib/live_view_studio_web/live/light_live.ex
+++ lib/live_view_studio_web/live/light_live.ex
@@ -3,35 +3,64 @@ defmodule LiveViewStudioWeb.LightLive do
 
   # a live view module ahs three calls back function
   # mount
-  def mount(_params,_session, socket) do
+  def mount(_params, _session, socket) do
     socket = assign(socket, brightness: 10)
     # mount returns a tuple with 2 elements
     {:ok, socket}
 
     # also the above can be wretten as a oneliner
-        # {:ok, assign(socket, brightness: 10)}
+    # {:ok, assign(socket, brightness: 10)}
   end
+
   # render
-def render(assigns) do
-  # ~H creates an Heex template or Html with embedded elixir
-  ~H"""
-  <h1>  Front Porch Light</h1>
-  <div id="light">
-    <div class="meter">
-      <span style={"width: #{@brightness}%"}>
-      <%= @brightness%>%
-      </span>
-      <button phx-click="off">
-        Off
+  def render(assigns) do
+    # ~H creates an Heex template or Html with embedded elixir
+    ~H"""
+    <h1>  Front Porch Light</h1>
+    <div id="light">
+      <div class="meter">
+        <span style={"width: #{@brightness}%"}>
+        <%= @brightness%>%
+        </span>
+        <button phx-click="off">
+          Off
+        </button>
+
+      <button phx-click="on">
+        On
+      </button>
+      <button phx-click="down">
+          Down
       </button>
 
-    <button phx-click="on">
-      On
-    </button>
+      <button phx-click="up">
+            Up
+      </button>
+      </div>
     </div>
-  </div>
-  """
+    """
+  end
 
-end
   # handle_event
+  def handle_event("on", _, socket) do
+    socket = assign(socket, :brightness, 100)
+    {:noreply, socket}
+  end
+
+  def handle_event("off", _, socket) do
+    socket = assign(socket, :brightness, 0)
+    {:noreply, socket}
+  end
+
+  def handle_event("down", _, socket) do
+    brightness = socket.assigns.brightness - 10
+    socket = assign(socket, :brightness, brightness)
+    {:noreply, socket}
+  end
+
+  def handle_event("up", _, socket) do
+    brightness = socket.assigns.brightness + 10
+    socket = assign(socket, :brightness, brightness)
+    {:noreply, socket}
+  end
 end
