2c091f2d66aafdee2ee3a7731a0e07a73db91c08
mik284 <mikeykamau222@gmail.com>
2023-12-25 23:13:14 +0300

    return a json instead of html

--- lib/live_view_studio_web/controllers/page_controller.ex
+++ lib/live_view_studio_web/controllers/page_controller.ex
@@ -11,11 +11,21 @@ defmodule LiveViewStudioWeb.PageController do
     render(conn, :LightLive)
   end
 
+    # def users(conn, _params) do
+    #   users = [
+    #     %{id: 1, name: "Alice", email: "alicemail@gmail.com"},
+    #      %{id: 2, name: "mike", email: "mikemail@gmail.com"},
+    #   ]
+    #   render(conn, :users, users: users)
+    # end
+
+    # how to return a json
   def users(conn, _params) do
     users = [
       %{id: 1, name: "Alice", email: "alicemail@gmail.com"},
        %{id: 2, name: "mike", email: "mikemail@gmail.com"},
     ]
-    render(conn, :users, users: users)
+
+    json(conn, %{users: users})
   end
 end
